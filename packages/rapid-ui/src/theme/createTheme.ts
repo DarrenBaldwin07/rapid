60fb0f0ec5f9505f2ecfa8de7f87aad9c814fdb0
Darren B <68653294+Devd0@users.noreply.github.com>
2023-02-06 10:09:35 -0500

    init themeing

--- packages/rapid-ui/src/theme/createTheme.ts
+++ packages/rapid-ui/src/theme/createTheme.ts
@@ -6,18 +6,46 @@ import {
 	StringToBoolean,
 	RapidUiThemeConfig,
 } from './types';
+import { RapidStyles } from '../utils';
 
-const generateClassNames = () => {};
 
-export const createVariant = () => {};
+export const createTheme = () => {};
 
-function createTheme<T, E>(config: RapidUiThemeConfig<T, E>) {
-	return config;
+function createVariant<T, E>(config: RapidUiThemeConfig<T, E>) {
+	const { variants, defaultProps, sizes, baseStyle } = config;
+
+	const typedSizes = sizes as E;
+
+	type Variant = keyof typeof variants;
+	type Size = keyof typeof typedSizes;
+
+	// Get the variant based on either the default variant or the variant that was passed in as a prop
+	const getVariantClassNames = (variant?: Variant) => variant ? variants[variant] : variants[defaultProps.variant];
+
+	const getSizeClassNames = (size?: Size): ClassValue | string => {
+		// Check if the consumer declared any sizes
+		if (sizes) {
+			// If they did then lets grab the classNames
+			return size ? sizes[size] as ClassValue : sizes[defaultProps.size] as ClassValue;
+		} else {
+			// Return an empty string with no size classNames
+			return '';
+		}
+	};
+
+	return (variant?: Variant, size?: Size) => {
+		let classNames = baseStyle
+		let sizeVariantClassNames = RapidStyles(getSizeClassNames(size), getVariantClassNames(variant) as string)
+
+		return RapidStyles(sizeVariantClassNames, baseStyle);
+	};
 }
 
-const theme = createTheme({
+const theme = createVariant({
+	baseStyle: 'bg-red-500',
 	variants: {
 		danger: 'text-red-500',
+		test: 'text-2'
 	},
 	sizes: {
 		lg: 'h-12',
@@ -28,4 +56,6 @@ const theme = createTheme({
 	},
 });
 
-export default createTheme;
+console.log(theme('danger', 'lg'))
+
+export default createVariant;
--- packages/rapid-ui/src/utils.ts
+++ packages/rapid-ui/src/utils.ts
@@ -1,8 +1,12 @@
 import { twMerge } from 'tailwind-merge';
+import { ClassValue } from './theme/types';
 
-const falsyToString = <T extends unknown>(value: T) =>
+export const falsyToString = <T extends unknown>(value: T) =>
 	typeof value === 'boolean' ? `${value}` : value === 0 ? '0' : value;
 
-export function RapidStyles(classes: string, defaults: string) {
+export const sanitizeClassNames = () => {
+
+}
+export function RapidStyles(classes: string | ClassValue, defaults: string | ClassValue) {
 	return twMerge(defaults, classes);
 }

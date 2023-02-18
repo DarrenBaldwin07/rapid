95c25ad68abcda15ba6beb6607e66e40ac4b0cd5
Darren B <68653294+Devd0@users.noreply.github.com>
2023-02-18 11:43:15 -0500

    defaultStyles for menuItems

--- packages/rapid-ui/src/components/overlay/menu/MenuItems.tsx
+++ packages/rapid-ui/src/components/overlay/menu/MenuItems.tsx
@@ -9,14 +9,14 @@ interface MenuItemsProps extends React.HTMLAttributes<HTMLDivElement> {
     styles?: string;
 }
 
-
-const MenuButton = ({ styles, ...rest }: MenuItemsProps) => {
+const MenuItems = ({ styles, ...rest }: MenuItemsProps) => {
+    const defaultStyles = 'bg-white border border-lightGrey absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none';
     return (
         <HeadlessMenu.Items
             {...rest}
-            className={RapidStyles(styles, THEME_CLASSNAME)}
+            className={RapidStyles(styles, defaultStyles, THEME_CLASSNAME)}
         />
     )
 };
 
-export default MenuButton;
+export default MenuItems;

753b09a76acb51635c3320def70f39c30c05896a
Darren B <68653294+Devd0@users.noreply.github.com>
2023-02-21 23:15:50 -0500

    updates

--- packages/rapid-ui/src/index.ts
+++ packages/rapid-ui/src/index.ts
@@ -1,5 +1,6 @@
 export * from './components';
 export * from './plugin';
 export * from './theme';
+export * from './hooks';
 export { default as classNames } from './conditional';
 export { TAILWIND_COMPONENTS_ENTRYPOINT as rapidStylesPath } from './constants';

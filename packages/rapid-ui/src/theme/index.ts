eec8b28ae27f88a4ee66dae2d05d433ee1d50fc5
Darren B <68653294+Devd0@users.noreply.github.com>
2023-03-01 09:57:21 -0500

    revert

--- packages/rapid-ui/src/theme/index.ts
+++ packages/rapid-ui/src/theme/index.ts
@@ -1,10 +1,4 @@
-import { ThemeSchema, RapidUiThemeConfig } from './types';
-import { VariantOutput } from './createTheme';
 export * from './types';
 export * from './createTheme';
 export { default as createVariant } from './createTheme';
 export { default as rapidTailwindTheme } from './tailwindTheme';
-
-export type ThemeVariant = <T extends ThemeSchema, E extends ThemeSchema>(
-	config: RapidUiThemeConfig<T, E>,
-) => VariantOutput;

b80058a4be35a76356131ab069d08087f02938b4
Darren B <68653294+Devd0@users.noreply.github.com>
2023-03-03 00:15:09 -0500

    add safe list comments

--- packages/rapid-ui/src/components/layout/Stack.tsx
+++ packages/rapid-ui/src/components/layout/Stack.tsx
@@ -16,6 +16,15 @@ interface StackProps
 	styles?: string;
 }
 
+
+// ------------------- TAILWINDCSS CLASSES TO FORCE JIT TO COMPILE -------------------
+// space-y-6
+// space-y-3
+// space-y-1.5
+// space-x-6
+// space-x-3
+// space-x-1.5
+
 const Stack = React.forwardRef<HTMLDivElement, StackProps>(
 	(
 		{ styles, children, spacing = 'lg', direction = 'column', ...rest },

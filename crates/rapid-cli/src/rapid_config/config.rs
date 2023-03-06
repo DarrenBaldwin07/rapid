02c49caad585ef5657d976e22ebe67c7e311bba8
Darren B <68653294+Devd0@users.noreply.github.com>
2023-03-05 22:09:55 -0500

    updates

--- crates/rapid-cli/src/rapid_config/config.rs
+++ crates/rapid-cli/src/rapid_config/config.rs
@@ -20,8 +20,8 @@ pub struct ServerConfig {
 }
 
 #[derive(Deserialize)]
-// Eventually rapid will have something called plugins -- for now "features" are simply optional internal functionality
-// that can be toggled by adding the desired feature to this "features" object inside of the rapid.toml config file
+/// Eventually rapid will have something called plugins -- for now "features" are simply optional internal functionality
+/// that can be toggled by adding the desired feature to this "features" object inside of the rapid.toml config file
 pub struct Features {
     // TODO: Add features here as needed
 }
@@ -35,17 +35,20 @@ pub struct RapidConfig {
     pub server: Option<ServerConfig>,
 }
 
-pub fn find_rapid_config() {
+pub fn find_rapid_config() -> RapidConfig {
     let dir = current_directory();
     // Look for the Rapid config file inside of the current working directory
     let config_file_contents = read_to_string(dir.join("rapid.toml"));
 
+    // Check to make sure that the config file did not throw an error
     if let Err(_) = config_file_contents {
-        // TODO: We should improve error log styling later
+        // TODO: We should improve error log styling later (just uses standard exit 200 best practices for now)
         eprintln!("Could not find a valid config file in the current working directory. Please make sure you are in a project scaffolded with the Rapid CLI.");
         std::process::exit(200);
     }
 
     // Parse/deserialize the rapid config file from the .toml format
     let rapid_config: RapidConfig = toml::from_str(&config_file_contents.unwrap()).unwrap();
+
+    rapid_config
 }

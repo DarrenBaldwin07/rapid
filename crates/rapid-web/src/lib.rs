3786ea3ae01e0ff654dec9352bdd5598f1d77d25
Darren B <68653294+Devd0@users.noreply.github.com>
2023-03-07 09:59:15 -0500

    init

--- crates/rapid-web/src/lib.rs
+++ crates/rapid-web/src/lib.rs
@@ -1,5 +1,8 @@
-pub use actix_web as server;
+pub use actix_web as actix;
 pub use actix_files as files;
+pub use actix_cors as cors;
+pub use actix_web_httpauth as auth;
+pub mod server;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
--- crates/rapid-web/src/server.rs
+++ crates/rapid-web/src/server.rs
@@ -1,4 +1,24 @@
+use super::actix::{web, App, HttpServer};
+use super::cors::Cors;
 
-pub struct RapidServer {
+pub struct RapidServer<T> {
+    pub port: Option<u16>,
+    pub is_logging: Option<bool>,
+    pub app_state: Option<T>,
+    pub cors: Option<Cors>
+}
 
+impl<T> RapidServer<T> {
+    fn create(port: Option<u16>, is_logging: Option<bool>, app_state: Option<T>, cors: Option<Cors>) -> Self {
+        Self {
+            port,
+            is_logging,
+            app_state,
+            cors
+        }
+    }
+
+    fn listen() {
+
+    }
 }

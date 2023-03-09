fb6b9156374c2ded4f0790f4c063a52d0973170a
Waldo <waldorabie@gmail.com>
2023-03-09 17:30:51 -0500

    Add Flex component

---
+++ packages/rapid-ui/src/components/primitives/flex/Flex.tsx
@@ -0,0 +1,31 @@
+import { forwardRef, DetailedHTMLProps, HTMLAttributes } from 'react';
+import { RapidStyles, getValidReactChildren } from '../../../utils';
+
+const RAPID_CLASSNAME = 'rapid-flex';
+
+interface FlexProps
+	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+	styles?: string;
+}
+
+const Flex = forwardRef<HTMLDivElement, FlexProps>(
+	({ styles, children, ...rest }, ref) => {
+		const flexStyles = 'flex';
+
+		return (
+			<div
+				ref={ref}
+				{...rest}
+				className={RapidStyles(
+					styles || rest.className,
+					flexStyles,
+					RAPID_CLASSNAME,
+				)}
+			>
+				{children}
+			</div>
+		);
+	},
+);
+
+export default Flex;
--- packages/rapid-ui/src/components/primitives/index.ts
+++ packages/rapid-ui/src/components/primitives/index.ts
@@ -4,3 +4,4 @@ export * from './heading';
 export * from './input';
 export * from './textarea';
 export * from './select';
+export * from './flex';

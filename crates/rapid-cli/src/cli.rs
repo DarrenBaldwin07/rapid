3cf95b0c3b92d0e406558d075a149183c7b64335
Darren B <68653294+Devd0@users.noreply.github.com>
2023-03-27 23:42:52 -0400

    trigger releases

--- crates/rapid-cli/src/cli.rs
+++ crates/rapid-cli/src/cli.rs
@@ -97,7 +97,7 @@ impl RapidCLI {
 	}
 }
 
-// TODO: update this to actually be a legit health templates
+// TODO: update this to actually be a legit health template
 pub fn get_help_template() -> &'static str {"The cammand line tool for the RAPID framework.
 
 Options:
--- crates/rapid-cli/src/commands/new.rs
+++ crates/rapid-cli/src/commands/new.rs
@@ -1,7 +1,12 @@
-use super::RapidCommand;
-use crate::cli::{logo, Config};
+use super::RapidCommand;use crate::{
+	cli::{current_directory, logo, rapid_logo, Config},
+	constants::BOLT_EMOJI,
+};
 use clap::{arg, value_parser, ArgAction, ArgMatches, Command};
+use colorful::{Color, Colorful};
+use toml::ser;
 use std::path::PathBuf;
+use include_dir::{include_dir, Dir};
 
 pub struct New {}
 
@@ -29,8 +34,51 @@ impl RapidCommand for New {
 
 	fn execute(_: &Config, args: &ArgMatches) -> Result<(), crate::cli::CliError<'static>> {
 		println!("{}", logo());
-		println!("> Creating new Rapid project in current working dir!");
-		println!("{:?}", args.get_one::<PathBuf>("fullstack"));
+		parse_new_args(args);
 		Ok(())
 	}
+}
+
+
+pub fn parse_new_args(args: &ArgMatches) {
+	/// NOTE: We can add more args for templates here (ideally we add nextjs asap)
+	const NEW_ARGS: [&str; 2] = ["fullstack", "server"];
+	// Get the current working directory of the user
+	let current_working_directory = current_directory();
+
+	for arg in NEW_ARGS {
+		match args.get_one::<PathBuf>(arg) {
+			Some(val) => {
+				if val == &PathBuf::from("true") {
+					match arg {
+						"fullstack" => {
+							init_fullstack_template(current_working_directory, arg);
+							break;
+						}
+						"server" => {
+							init_server_template(current_working_directory, arg);
+							break;
+						}
+						_ => {
+							println!("> Invalid argument passed to new command!");
+							break;
+						}
+					}
+				}
+			}
+			None => {
+				println!("> No argument passed to new command!");
+				break;
+			}
+		}
+	}
+}
+
+pub fn init_fullstack_template(current_working_directory: PathBuf, arg: &str) {
+	println!("Coming soon...");
+}
+
+pub fn init_server_template(current_working_directory: PathBuf, arg: &str) {
+	include_dir!("crates/rapid-cli/src/templates/server").extract(current_working_directory).unwrap();
+	println!("{} {:?}...", "Initializing a new rapid-web server application".color(Color::Green), arg);
 }

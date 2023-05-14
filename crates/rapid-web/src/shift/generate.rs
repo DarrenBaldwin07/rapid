0f73f0da220044b8dd526aa3e88ea33b0fd04bbd
Darren B <68653294+Devd0@users.noreply.github.com>
2023-05-14 17:21:48 -0400

    type gen refactors

--- crates/rapid-web/src/shift/generate.rs
+++ crates/rapid-web/src/shift/generate.rs
@@ -143,6 +143,18 @@ pub fn generate_handler_types(routes_path: PathBuf, converter: &mut TypescriptCo
 					route_key,
 				}));
 			}
+			HandlerRequestType::Query => {
+				handlers.push(Handler::Query(TypedQueryHandler {
+					request_type,
+					path,
+					query_params,
+					output_type: TypescriptType {
+						typescript_type: "any".to_string(),
+						is_optional: true,
+					},
+					route_key,
+				}));
+			}
 			_ => {
 				handlers.push(Handler::Mutation(TypedMutationHandler {
 					request_type,

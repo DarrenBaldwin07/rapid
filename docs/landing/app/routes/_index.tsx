c58c2de31db5e45ae0d4d13c9db70c56370946d2
Darren B <68653294+Devd0@users.noreply.github.com>
2023-05-28 23:32:05 -0400

    banner btn

--- docs/landing/app/routes/_index.tsx
+++ docs/landing/app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 import Layout from "~/components/Layout";
+import { Heading, Text } from "@rapid-web/ui";
+import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -11,7 +15,20 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <Layout isNavigation>
-      <h1 className='text-white'>Hello world!</h1>
+      <div>
+        <motion.div
+          animate={{ y: 0, visibility: 'visible', opacity: 1 }}
+          initial={{ y: -70, visibility: 'hidden', opacity: 0 }}
+          transition={{ duration: 1, delay: 0 }}
+        >
+          <div className='flex flex-col mt-56'>
+            <div>
+              <Text styles='text-white'>Announcement blog post</Text>
+            </div>
+            <Heading styles='text-white font-extrabold text-6xl max-w-[1000px] gradient-text leading-tight mt-12'>A new way to build web applications with React and Rust.</Heading>
+          </div>
+        </motion.div>
+      </div>
     </Layout>
   );
 }

8e7215c8482e9bd3da80cc1fe09462f1b8211f57
paynejj <joshua.payne.j@gmail.com>
2023-07-18 13:44:53 -0400

    Add defaultStyles to Box.tsx

--- packages/rapid-ui/src/components/primitives/box/Box.tsx
+++ packages/rapid-ui/src/components/primitives/box/Box.tsx
@@ -1,35 +1,36 @@
-import React from "react";
-import { RapidStyles } from "../../../utils";
+import React from 'react';
+import { RapidStyles } from '../../../utils';
 
-type BoxType = "div";
+type BoxType = 'div';
 
 interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
-  styles?: string;
-  as?: BoxType;
+	styles?: string;
+	as?: BoxType;
 }
 
-const THEME_CLASSNAME = "rapid-box";
+const THEME_CLASSNAME = 'rapid-box';
 
 const Box = React.forwardRef<HTMLDivElement, BoxProps>(
-  ({ styles, as = "div", ...rest }, ref) => {
-    const Component = (type: BoxType) => {
-      switch (type) {
-        case "div":
-          return (
-            <div
-              ref={ref}
-              className={RapidStyles(
-                styles || rest.className,
-                THEME_CLASSNAME,
-              )}
-            />
-          );
-      }
-    };
-    return Component(as);
-  },
+	({ styles, as = 'div', ...rest }, ref) => {
+		const Component = (type: BoxType) => {
+			switch (type) {
+				case 'div':
+					return (
+						<div
+							ref={ref}
+							className={RapidStyles(
+								styles || rest.className,
+								'', //defaultStyles
+								THEME_CLASSNAME,
+							)}
+						/>
+					);
+			}
+		};
+		return Component(as);
+	},
 );
 
-Box.displayName = "Box";
+Box.displayName = 'Box';
 
 export default Box;

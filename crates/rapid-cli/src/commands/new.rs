c0f535a574173ec16a19e780a7026458c89e1a48
Darren B <68653294+Devd0@users.noreply.github.com>
2023-07-20 00:25:35 -0400

    nextjs support complete

--- crates/rapid-cli/src/commands/new.rs
+++ crates/rapid-cli/src/commands/new.rs
@@ -19,6 +19,7 @@ use std::{
 // We need to get the project directory to extract the template files (this is because include_dir!() is yoinked inside of a workspace)
 static PROJECT_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/templates/server");
 static REMIX_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/templates/remix");
+static NEXTJS_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/templates/nextjs");
 static REMIX_WITHOUT_CLERK_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/templates/remix-without-clerk");
 
 pub struct New {}
@@ -31,6 +32,8 @@ impl RapidCommand for New {
 				Arg::new("remix")
 					.long("remix")
 					.required(false)
+					.conflicts_with("nextjs")
+					.conflicts_with("server")
 					.action(ArgAction::SetTrue)
 					.value_name("REMIX")
 					.help("Scaffolds a fullstack Rapid project with remix!"),
@@ -39,6 +42,8 @@ impl RapidCommand for New {
 				Arg::new("nextjs")
 					.long("nextjs")
 					.required(false)
+					.conflicts_with("server")
+					.conflicts_with("remix")
 					.action(ArgAction::SetTrue)
 					.value_name("NEXTJS")
 					.help("Scaffolds a fullstack Rapid project with Nextjs!"),
@@ -47,6 +52,8 @@ impl RapidCommand for New {
 				Arg::new("server")
 					.long("server")
 					.required(false)
+					.conflicts_with("nextjs")
+					.conflicts_with("remix")
 					.action(ArgAction::SetTrue)
 					.value_name("SERVER")
 					.help("Scaffolds a server-side only Rapid project!"),
@@ -66,20 +73,22 @@ pub fn parse_new_args(args: &ArgMatches) {
 
 	let is_remix = args.get_one::<bool>("remix").unwrap_or(&true);
 	let is_server = args.get_one::<bool>("server").unwrap_or(&false);
+	let is_nextjs = args.get_one::<bool>("nextjs").unwrap_or(&false);
 
-	match (is_remix, is_server) {
-		(true, false) => {
+	match (is_remix, is_server, is_nextjs) {
+		(false, false, true) => {
+			init_nextjs_template(current_working_directory);
+		}
+		(true, false, false) => {
 			init_remix_template(current_working_directory);
 		}
-		(false, true) => {
+		(false, true, false) => {
 			init_server_template(current_working_directory, "server");
 		}
-		(true, true) => {
+		(false, false, false) => {
 			init_remix_template(current_working_directory);
 		}
-		(false, false) => {
-			init_remix_template(current_working_directory);
-		}
+		_ => unreachable!(),
 	}
 }
 
@@ -353,5 +362,151 @@ pub fn init_server_template(current_working_directory: PathBuf, _: &str) {
 		BOLT_EMOJI,
 		format!("\n\ncd {}", project_name).bold(),
 		"\nrapid run".bold()
+	);
+}
+
+
+pub fn init_nextjs_template(current_working_directory: PathBuf) {
+	// Ask the user what they want to name their project
+	let project_name = prompt_one(
+		Question::input("project_name")
+			.message("What will your project be called?")
+			.default("my-app")
+			.build(),
+	)
+	.expect("Error: Could not scaffold project. Please try again!");
+
+	let project_name = project_name.as_string().unwrap();
+
+	// Validate that the project name does not contain any invalid chars
+	if !project_name.chars().all(|x| x.is_alphanumeric() || x == '-' || x == '_') {
+		println!("Aborting...your project name may only contain alphanumeric characters along with '-' and '_'...");
+		exit(64);
+	}
+
+	let path = current_working_directory.join(project_name);
+
+	// Check if the path already exists (if it does we want to ask the user if they want to delete it)
+	if path.exists() {
+		let force = prompt_one(
+			Question::confirm("force_delete")
+				.message("Your specified directory is not empty and has files currently in it, do you want to overwrite?")
+				.default(false)
+				.build(),
+		)
+		.expect("Error: Could not scaffold project. Please try again!");
+
+		match !force.as_bool().unwrap() {
+			true => {
+				exit(64);
+			}
+			false => {
+				remove_dir_all(&path).expect("Error: Could not scaffold project. The specified directory must be empty. Please try again!");
+			}
+		}
+	}
+
+	// Before we initialize the project, lets ask the user to specify which package manager they want to use and then run the install process with that package manager
+	let manager_choices = vec!["pnpm", "npm", "yarn"];
+
+	println!("{}", indent(1));
+
+	let package_manager = requestty::Question::select("packageManagerSelect")
+		.message("Which package manager would you like to use?:")
+		.choices(manager_choices)
+		.page_size(6)
+		.build();
+
+	let package_manager = prompt(vec![package_manager]).expect("Error: Could not scaffold project. Please try again!");
+
+	let package_manager = match package_manager.get("packageManagerSelect") {
+		Some(Answer::ListItem(choice)) => choice.text.clone(),
+		_ => {
+			println!(
+				"{}",
+				"Aborting...an error occurred while trying to parse package manager selection. Please try again!"
+					.bold()
+					.color(Color::Red)
+			);
+			exit(64);
+		}
+	};
+
+	println!("{}", indent(1));
+
+	let loading = Spinach::new(format!("{}", "Initializing a new Rapid Nextjs application..".color(Color::LightCyan)));
+
+	// Run the scaffold commands
+	StdCommand::new("sh")
+		.current_dir(current_directory())
+		.arg("-c")
+		.arg(format!("mkdir {}", project_name))
+		.spawn()
+		.unwrap()
+		.wait()
+		.expect("Error: Could not scaffold project. Please try again!");
+
+	// Initialize a git repo
+	StdCommand::new("sh")
+		.current_dir(current_directory().join(project_name))
+		.arg("-c")
+		.arg(format!("git init --quiet"))
+		.spawn()
+		.unwrap()
+		.wait()
+		.expect("Error: Could not scaffold project. Please try again!");
+
+	// Replace the default source dir with our own template files
+	NEXTJS_DIR.extract(current_working_directory.join(project_name).clone()).unwrap();
+
+	// Rename cargo.toml file (We have to set it to Cargo__toml due to a random bug with cargo publish command in a workspace)
+	StdCommand::new("sh")
+		.current_dir(current_directory().join(project_name))
+		.arg("-c")
+		.arg(format!("mv Cargo__toml Cargo.toml"))
+		.spawn()
+		.unwrap()
+		.wait()
+		.expect("Error: Could not scaffold project. Please try again!");
+
+	// Sleep a little to show loading animation, etc (there is a nice one we could use from the "tui" crate)
+	let timeout = time::Duration::from_millis(1000);
+	thread::sleep(timeout);
+
+	// stop showing the loader
+	loading.succeed("Initialized!");
+
+	// Take the package manager and run the install command
+	let loading = Spinach::new(format!("{}", "Installing dependencies...".color(Color::LightCyan)));
+
+	StdCommand::new("sh")
+		.current_dir(current_directory().join(project_name))
+		.arg("-c")
+		.arg(format!("{} install > /dev/null 2>&1", package_manager))
+		.spawn()
+		.unwrap()
+		.wait()
+		.expect("Error: Could not install project dependencies!");
+
+	loading.succeed("Installed dependencies!");
+
+	clean_console();
+
+	println!(
+		"\n\n{} {} {} {}",
+		format!("{}", rapid_logo()).bold(),
+		"Success".bg_blue().color(Color::White).bold(),
+		BOLT_EMOJI,
+		"Welcome to your new Rapid application with Nextjs!"
+	);
+
+	println!(
+		"{} {} {} {} {} {}",
+		"\n\n🚀".bold(),
+		"Next Steps".bg_blue().color(Color::White).bold(),
+		BOLT_EMOJI,
+		format!("\n\ncd {}", project_name).bold(),
+		"\nrapid run".bold(),
+		format!("\n{} run dev", package_manager).bold()
 	);
 }

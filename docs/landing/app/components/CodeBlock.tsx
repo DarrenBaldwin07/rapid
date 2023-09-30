0db03c5c8b60f27b66fc7f16c5556cda0e4014f2
Darren B <68653294+Devd0@users.noreply.github.com>
2023-09-30 10:07:07 -0400

    add fileName tag on code field (optional)

--- docs/landing/app/components/CodeBlock.tsx
+++ docs/landing/app/components/CodeBlock.tsx
@@ -10,9 +10,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 interface Props {
 	language: string;
 	code: string;
+	fileName?: string;
 }
 
-const CodeBlock = ({ language, code }: Props) => {
+const CodeBlock = ({ language, code, fileName }: Props) => {
 	const codeRef = useRef<HTMLDivElement>(null);
 	const [isShowingCopy, setIsShowingCopy] = useState(false);
 	const [isCopied, setIsCopied] = useState(false);
@@ -27,33 +28,43 @@ const CodeBlock = ({ language, code }: Props) => {
 			onMouseOver={() => setIsShowingCopy(true)}
 			onMouseLeave={() => setTimeout(() => setIsShowingCopy(false), 250)}
 		>
-			<div className='flex w-full justify-between rounded-lg bg-[#282C34]'>
-				<pre ref={codeRef as any} className={`language-${language}`}>
-					{code}
-				</pre>
-				<div>
-					{isShowingCopy && (
-						<button
-							className='m-2 flex self-end rounded-lg border border-[#27272D] bg-[#18181C] p-2 text-white transition-all duration-100 ease-linear'
-							onClick={() => {
-								navigator.clipboard.writeText(code);
-								setIsShowingCopy(false);
-								setIsCopied(true);
-								setTimeout(() => {
-									setIsCopied(false);
-								}, 1000);
-							}}
-						>
-							<FontAwesomeIcon
-								icon={isCopied ? faCheck : faCopy}
-								color='white'
-								size='sm'
-								width={16}
-								height={16}
-							/>
-						</button>
-					)}
+			<div className='flex flex-col rounded-lg bg-[#282C34] p-2'>
+				<div className='flex w-full justify-between'>
+					<pre
+						ref={codeRef as any}
+						className={`language-${language} text-sm`}
+					>
+						{code}
+					</pre>
+					<div>
+						{isShowingCopy && (
+							<button
+								className='m-2 flex self-end rounded-lg border border-[#27272D] bg-[#18181C] p-2 text-white transition-all duration-100 ease-linear'
+								onClick={() => {
+									navigator.clipboard.writeText(code);
+									setIsShowingCopy(false);
+									setIsCopied(true);
+									setTimeout(() => {
+										setIsCopied(false);
+									}, 1000);
+								}}
+							>
+								<FontAwesomeIcon
+									icon={isCopied ? faCheck : faCopy}
+									color='white'
+									size='sm'
+									width={16}
+									height={16}
+								/>
+							</button>
+						)}
+					</div>
 				</div>
+				{fileName && (
+					<div className='w-max self-end rounded-full bg-black px-2 py-1 text-xs text-white'>
+						<pre>{fileName}</pre>
+					</div>
+				)}
 			</div>
 		</div>
 	);
